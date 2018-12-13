dcc0ad9ea9f6bacb6dcaa3ac3eac62599f385750
ilyamakhon <i.makhon@yandex.ru>
2018-12-13 23:33:49 +0300

    variables fixes

--- src/test/java/com/epam/ht2/PageObjectTest.java
+++ src/test/java/com/epam/ht2/PageObjectTest.java
@@ -14,8 +14,11 @@ public class PageObjectTest {
     private static final String BASE_URL = "http://localhost:8080";
     private static final String WEB_DRIVER = "webdriver.chrome.driver";
     private static final String WEB_DRIVER_LOCAL_URL = "C://Program Files (x86)//Google//chromedriver.exe";
+    private static final String JENKINS_LOGIN = "ilyamakhon";
+    private static final String JENKINS_PASSWORD = "qwerty123#@!";
+
     private WebDriver driver = null;
-    private boolean elementFound;
+    private boolean isElementFound;
 
     private String expected;
 
@@ -37,34 +40,34 @@ public class PageObjectTest {
         PageObjectSample page = PageFactory.initElements(driver, PageObjectSample.class);
 
         //1-step: authorization
-        page.setJUsername("ilyamakhon");
-        Assert.assertEquals(page.getJUsernameValue(), "ilyamakhon", "Unable to fill 'Username' field");
+        page.setJUsername(JENKINS_LOGIN);
+        Assert.assertEquals(page.getJUsernameValue(), JENKINS_LOGIN, "Unable to fill 'Username' field");
 
-        page.setJPassword("qwerty123#@!");
-        Assert.assertEquals(page.getJPasswordValue(), "qwerty123#@!", "Unable to fill 'Password' field");
+        page.setJPassword(JENKINS_PASSWORD);
+        Assert.assertEquals(page.getJPasswordValue(), JENKINS_PASSWORD, "Unable to fill 'Password' field");
 
         page.submitForm();
 
         //2-step: detecting and clicking 'Manage Jenkins' link
-        elementFound = page.isElementPresent(page.getManageJenkinsLink());
-        Assert.assertTrue(elementFound, "Element 'a' with text value = 'Manage Jenkins' not found on this page");
+        isElementFound = page.isElementPresent(page.getManageJenkinsLink());
+        Assert.assertTrue(isElementFound, "Element 'a' with text value = 'Manage Jenkins' not found on this page");
         page.manageJenkins();
 
         //3-step: detecting and clicking 'Manage Users' link
-        elementFound = page.isElementPresent(page.getManageUsersLink());
-        Assert.assertTrue(elementFound, "Element 'a' with text value = 'Manage Users' not found on this page");
+        isElementFound = page.isElementPresent(page.getManageUsersLink());
+        Assert.assertTrue(isElementFound, "Element 'a' with text value = 'Manage Users' not found on this page");
         //Detecting describing 'Manage User' link element
         expected = "Create/delete/modify users that can log in to this Jenkins";
         Assert.assertEquals(page.getManageUsersDescriptionElement(), expected, "Element 'dd' with text: '" + expected + "' not found on this page");
         page.manageUsers();
 
         //4-step: detecting and clicking 'Create User' link
-        elementFound = page.isElementPresent(page.getCreateUserLink());
-        Assert.assertTrue(elementFound, "Element 'Create User' not found on this page");
+        isElementFound = page.isElementPresent(page.getCreateUserLink());
+        Assert.assertTrue(isElementFound, "Element 'Create User' not found on this page");
         page.createUserLink();
         //Detecting form for creating a new user
-        elementFound = page.isFormPresent();
-        Assert.assertTrue(elementFound, "Form for creating new user was not detected on this page");
+        isElementFound = page.isFormPresent();
+        Assert.assertTrue(isElementFound, "Form for creating new user was not detected on this page");
 
         //5-step: creating new user after detecting form
         page.setNewUserName("someuser")
@@ -89,13 +92,13 @@ public class PageObjectTest {
          */
         page.confirmSomeUserDelition();
 
-        elementFound = page.isElementPresent(page.getSomeUserTdElement());
-        Assert.assertFalse(elementFound, "Element tr->td with text='someuser' still exists");
-        elementFound = page.isElementPresent(page.getDeleteSomeUserLink());
-        Assert.assertFalse(elementFound, "Element tr->td->a with href='user/someuser/delete' still exists");
+        isElementFound = page.isElementPresent(page.getSomeUserTdElement());
+        Assert.assertFalse(isElementFound, "Element tr->td with text='someuser' still exists");
+        isElementFound = page.isElementPresent(page.getDeleteSomeUserLink());
+        Assert.assertFalse(isElementFound, "Element tr->td->a with href='user/someuser/delete' still exists");
 
         //8-step: check that element 'a' with href='user/admin/delete' does not exists
-        elementFound = page.isElementPresent(page.getUserAdminDeleteLink());
-        Assert.assertFalse(elementFound, "Element 'a' with href='user/admin/delete' exists");
+        isElementFound = page.isElementPresent(page.getUserAdminDeleteLink());
+        Assert.assertFalse(isElementFound, "Element 'a' with href='user/admin/delete' exists");
     }
 }
